24d0c9887c23b677d6df8a379953fc14451b18cc
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2022-11-16 15:48:26 -0800

    add fields and enable wait_on_rate_limit

--- crawler/twitter_covid19_api_v2_crawler.py
+++ crawler/twitter_covid19_api_v2_crawler.py
@@ -35,11 +35,11 @@ class TweetCOVID19APIV2Crawler(CrawlerBase):
         class V2Client(tweepy.StreamingClient):
 
             # This will print the Tweet ID and Tweet text for each Tweet
-            def on_tweet(self, tweet):
-                extractor.export([tweet], file_name="coronavirus")
+            def on_data(self, data):
+                extractor.export([data.decode('utf-8')], file_name="coronavirus")
 
         # Replace with your own bearer token below
-        self.client = V2Client(self.get_bearer_token())
+        self.client = V2Client(self.get_bearer_token(), wait_on_rate_limit = True)
         # This is where we set our filter rule
         self.client.add_rules(tweepy.StreamRule("context:123.1220701888179359745"))
 
@@ -57,9 +57,20 @@ class TweetCOVID19APIV2Crawler(CrawlerBase):
         return body['access_token']
 
     def crawl(self):
-        self.client.filter()
+        self.client.filter(
+            tweet_fields="attachments,author_id,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,public_metrics,referenced_tweets,source,text,withheld,context_annotations,conversation_id,reply_settings".split(
+                ","),
+            user_fields="id,created_at,entities,username,name,location,url,verified,profile_image_url,public_metrics,pinned_tweet_id,description,protected".split(
+                ","),
+            expansions="author_id,entities.mentions.username,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id,attachments.media_keys,geo.place_id".split(
+                ","),
+            place_fields="contained_within,country,country_code,full_name,geo,id,name,place_type".split(","),
+            media_fields="duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width,alt_text,variants".split(
+                ",")
+        )
 
 
 if __name__ == '__main__':
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler())
+
